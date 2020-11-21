09f718125325ace4adbce2c2477f3f89b060935b
Zachary Gilliom <zacharygilliom@gmail.com>
2020-11-20 22:37:21 -0500

    Added some color layouts and added desktop database path.

--- src/app.py
+++ src/app.py
@@ -5,9 +5,10 @@ import dash_html_components as html
 import plotly.express as px
 
 #-------- Import and clean our Dataframe ------------
-PATH = "/home/zacharygilliom/pythonProjects/video-game-sales/backend/database/vgsales.db"
 
-salesData = VideoGameSales(PATH)
+# LAPTOPPATH = "/home/zacharygilliom/pythonProjects/video-game-sales/backend/database/vgsales.db"
+DESKTOPPATH= "/mnt/c/Users/zacha/Documents/pythonProjects/video-game-sales/backend/database/vgsales.db"
+salesData = VideoGameSales(DESKTOPPATH)
 
 salesData.makeDataframe()
 salesData.cleanDataframe()
@@ -19,11 +20,33 @@ df_melted = salesData.meltDataframe(df_grouped_year)
 external_stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 
+colors = {'background': '#336699',
+            'text': '#000000'}
+
 fig_bar = px.bar(df_melted, x='Year', y='Total_Sales', color='Sales_Region', barmode='group')
+
+fig_bar.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
 fig_line = px.line(df_melted, x='Year', y='Total_Sales', color='Sales_Region')
 
-app.layout = html.Div(children=[
-    html.H1('Video Game Sales Data'),
+fig_line.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
+    html.H1(
+        'Video Game Sales Data',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
     dcc.Graph(
         id='line-graph',
         figure=fig_line,
